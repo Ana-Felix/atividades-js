9885b23fbef442ede4928e79529f27a3e7c25017
Ana-Felix <92644868+Ana-Felix@users.noreply.github.com>
2021-11-28 18:10:26 -0300

    Add files via upload

---
+++ atividade6.js
@@ -0,0 +1,15 @@
+//
+// Função que retorna o dobro de um número qualquer
+//
+var dobro = function(num) {
+    return num * 2;
+}
+
+//
+// Testes
+//
+try {
+    assert.equal(10, dobro(5));
+} catch(e) {
+    console.log(e);
+}
